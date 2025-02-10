b0ac0c799a2011e7faf50b96f90a7d2acb076fa7
AllyTaylor96 <allytaylor@hotmail.co.uk>
2025-02-10 19:04:32 +0000

    Sort by rough start index for each entity

--- src/generate_html.py
+++ src/generate_html.py
@@ -1,11 +1,22 @@
-import os
-
 from io_functions import load_json
 
 from airium import Airium
 
 # Load HTML colours
 html_colour_list = load_json('src/colours.json')['html_colour_list']
+
+def sort_entities(entities: dict, total_entities: list) -> list:
+    """
+    Sort entities by their average position in the transcript.
+    """
+    entity_positions = {}
+    for entity_name in total_entities:
+        start_word_indexes = [x['startWordIndex'] for x in entities if x['id'] == entity_name]
+        entity_positions[entity_name] = sum(start_word_indexes) / len(start_word_indexes)
+
+    # Return a list of entities sorted by their average position in the transcript
+    sorted_entities = sorted(entity_positions, key=entity_positions.get)
+    return sorted_entities
 
 def generate_html(file_id: str, transcript: dict, entities: dict) -> str:
     """
@@ -35,11 +46,17 @@ def generate_html(file_id: str, transcript: dict, entities: dict) -> str:
     annotated_transcript = " ".join(annotated_words)
 
 
+
+    # Need to roughly-sort the entities by their average position in the transcript
+    # Ie. average out startIndex of all entities, sort by that, then assign colours
+    sorted_entities = sort_entities(entities['annotations'], entities['metadata']['ids'])
+
     # Get all entity IDs and assign colours
+    # Probably need to add a check for if the entity is not in the list of colours
     entity_colours = {}
-    total_entities = entities['metadata']['ids']
-    for i, entity_id in enumerate(total_entities):
+    for i, entity_id in enumerate(sorted_entities):
         entity_colours[entity_id] = html_colour_list[i % len(html_colour_list)]
+
     
     # Generate HTML using Airium
     a = Airium()
@@ -57,7 +74,7 @@ def generate_html(file_id: str, transcript: dict, entities: dict) -> str:
                     transition: background-color 0.3s ease;
                 }
                 """)
-                for entity_id in total_entities:
+                for entity_id in sorted_entities:
                     a(f"""
                     .entity.{entity_id} {{
                         background-color: {entity_colours[entity_id]};
@@ -74,7 +91,7 @@ def generate_html(file_id: str, transcript: dict, entities: dict) -> str:
             with a.h1(id="MainTitle", klass="main_header"):
                 a(f"{file_id}: NER Highlighting")
             with a.div():
-                for entity_id in total_entities:
+                for entity_id in sorted_entities:
                     a.input(id=f"toggle-{entity_id}", type="checkbox", checked=True)
                     with a.label(id=f"label-{entity_id}", **{"for": f"toggle-{entity_id}"}, style=f"background-color: {entity_colours[entity_id]};"):
                         a(f" {entity_id} ")
@@ -83,7 +100,7 @@ def generate_html(file_id: str, transcript: dict, entities: dict) -> str:
             with a.p(id="transcript"):
                 a(annotated_transcript)  # Insert the formatted transcript
 
-            for entity_id in total_entities:
+            for entity_id in sorted_entities:
                 with a.script():
                     a(f"""
                     document.getElementById("toggle-{entity_id}").addEventListener("change", function() {{
